8a75e56aa10b8b1d0fd66b58978605aeecd9f67e
mohwildanhabibi <mohwildannhabibi@gmail.com>
2020-10-22 18:26:04 +0800

    ganti pakai req.params

--- routes/users.js
+++ routes/users.js
@@ -4,19 +4,26 @@ const axios = require('axios')
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-	console.log(req.query.page);
 	try{
-		axios.get("https://reqres.in/api/users",{
-			params: {
-				page: req.query.page
-			}
-		})
+		axios.get("https://reqres.in/api/users")
 			.then(response => res.json(response.data))
 			.catch(err => res.send(err));
 	}
 	catch(err){
 		console.error("Error ==> ", err);
-	}	
+	}
+});
+
+router.get('/:page', function(req, res, next) {
+	console.log(req.params);
+	try{
+		axios.get("https://reqres.in/api/users", {params:{page: req.params.page}})
+			.then(response => res.json(response.data))
+			.catch(err => res.send(err));
+	}
+	catch(err){
+		console.error("Error ==> ", err);
+	}
 });
 
 router.get('/coba', function(req, res, next) {
